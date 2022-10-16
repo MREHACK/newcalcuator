63508833297b49818b1bde127a3e2d45b4dedcdd
MREHACK <115871928+MREHACK@users.noreply.github.com>
2022-10-16 14:42:39 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,41 @@
+num1=int(input("enter frist number :"))
+
+opr=int(input("enter opration :"))
+
+num2=int(input("enter second number :"))
+
+while true :
+    
+    
+    if oper== "+" :
+    
+    sum=num1+num2
+    
+    print("the ansewer is , sum")
+    
+    elif oper == "-" :
+    
+        subs=num1-num2
+    
+    print("the ansewer is , subs")
+    
+    
+    elif oper == "*" :
+    
+        mul=num1*num2
+    
+    print("the ansewer is ,mul")
+    
+    
+elif oper == "/" :
+    
+        div=num1/num2
+    
+    print("the ansewer is , div")
+    
+    
+    
+    
+
+
+
